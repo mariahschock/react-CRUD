a46471b1739c7288d16be42a9b03def0ac5def0f
Mariah <mariahschock@gmail.com>
2022-06-25 07:57:13 -0700

    auth page forms

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -2,6 +2,32 @@ import React from 'react';
 
 export default function AuthPage() {
   return (
-    <div>AuthPage</div>
+    <div>
+      <h2>Song Track</h2>
+      <form>
+        <h3>Sign In</h3>
+        <label>
+            email
+          <input type="email" />
+        </label>
+        <label>
+            password
+          <input type="password" />
+        </label>
+        <button>Sign In</button>
+      </form>
+      <form>
+        <h3>Sign Up</h3>
+        <label>
+            email
+          <input type="email" />
+        </label>
+        <label>
+            password
+          <input type="password" />
+        </label>
+        <button>Sign Up</button>
+      </form>
+    </div>
   );
 }
